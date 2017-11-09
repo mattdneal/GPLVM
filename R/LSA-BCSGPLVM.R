b6aa4fb05416d0270a92e5830b9260677611e7b9
mneal <mattdneal@gmail.com>
2017-11-09 15:58:24 +0000

    Bugfixes to 0.3.4

--- R/LSA-BCSGPLVM.R
+++ R/LSA-BCSGPLVM.R
@@ -333,8 +333,9 @@ array_to_flat_matrix <- function(data.array) {
 LSA_BCSGPLVM.plot_iteration <- function(A.hist, par.hist, plot.Z, iteration, classes, K.bc, max.iterations=1000) {
   close.screen(all.screens=TRUE)
   num.pars <- ncol(par.hist)
-  num.iterations <- nrow(par.hist)
-  iteration.range <- c(max(1, num.iteration - max.iterations + 1), num.iterations)
+  iteration.range <- max(1, iteration - max.iterations + 1):iteration
+  iteration.lims <- range(iteration.range)
+  iteration.lims[1] <- iteration.lims[1] - 1
   if (plot.Z) {
     split.screen(c(1, 2))
     par.screens <- split.screen(c(num.pars + 1, 1), 1)
@@ -359,13 +360,20 @@ LSA_BCSGPLVM.plot_iteration <- function(A.hist, par.hist, plot.Z, iteration, cla
   }
   par.hist <- par.hist[1:iteration, ]
   par.hist[, ncol(par.hist)] <- smoothed.L
+
+  # Do the legend first so we can plot the bottom axis over it.
+  screen(par.screens[length(par.screens)])
+  par(mar=c(0,0,0,0))
+  legend("center", legend=c(paste("It.", iteration), "alpha", "tau", "l_Z", paste("l_S", 1:(ncol(par.hist) - 4), sep=""), "L"),
+         col=c(0, viridis::viridis(ncol(par.hist), end=0.8)), lty=1, ncol=4, bty="n")
+
   for (i in 1:ncol(par.hist)) {
     screen(par.screens[i])
     par(mar=c(0,4,0,4)+0.1)
     plot(iteration.range, par.hist[iteration.range, i],
-         type="l", xlim=c(0, iteration.range[2]),
+         type="l", xlim=iteration.lims,
          ylim=range(par.hist[iteration.range, i]),
-         axes=F, ann=F, col=viridis::viridis(ncol(par.hist))[i])
+         axes=F, ann=F, col=viridis::viridis(ncol(par.hist), end=0.8)[i])
     box()
     range <- range(par.hist[iteration.range, i])
     at <- mean(range)
@@ -375,16 +383,11 @@ LSA_BCSGPLVM.plot_iteration <- function(A.hist, par.hist, plot.Z, iteration, cla
     at <- mean(range)
     labels <- format(par.hist[iteration, i], digits=2)
     axis(4, at=at, labels=labels, las=1, tick=FALSE)
+    if (i == ncol(par.hist)) axis(1, at=iteration.lims)
     par(mar=c(5.1,4.1,4.1,2.1))
   }
-  screen(par.screens[length(par.screens)])
-  par(mar=c(0,0,0,0))
-  legend("center", legend=c(paste("It.", iteration), "alpha", "tau", "l_Z", paste("l_S", 1:(ncol(par.hist) - 4), sep=""), "L"),
-         col=c(0, viridis::viridis(ncol(par.hist))), lty=1, ncol=4, bty="n")
-
-
   if (plot.Z) {
-    class.colours <- viridis::viridis(max(classes))[classes]
+    class.colours <- viridis::viridis(max(classes), end=0.8)[classes]
     if (q > 2) {
       screen.num <- 0
       for (i in 1:(q-1)) {
